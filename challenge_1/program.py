6e7a71b95d1562be6101e3a1a441fca8f5cd41dc
Gaston <60296343+gastonoterom@users.noreply.github.com>
2021-12-01 17:26:34 -0300

    final

--- challenge_1/program.py
+++ challenge_1/program.py
@@ -1,5 +1,5 @@
 nums = [int(i) for i in open("nums.txt").read().splitlines()]
-part_1 = sum(b>a for a,b in zip(nums[:len(nums)-1], nums[1:]))
+part_1 = sum(nums[i] > nums[i-1] for i in range(1,len(nums)))
 part_2 = sum(sum(nums[i-3:i]) > sum(nums[i-4:i-1]) for i in range(4,len(nums)+1))
 print(part_1)
 print(part_2)
