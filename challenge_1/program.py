8381c880107eb8ec240ced92f198b873005f80ae
Gaston <60296343+gastonoterom@users.noreply.github.com>
2021-12-01 17:23:17 -0300

    fixed

--- challenge_1/program.py
+++ challenge_1/program.py
@@ -1,7 +1,7 @@
-input=[int(l) for l in open("nums.txt").read().splitlines()]
+nums = [int(i) for i in open("nums.txt").read().splitlines()]
 
-part_1 = sum(b>a for a,b in zip(input[:len(input)-1], input[1:]))
-part_2 = sum(b>a for a,b in zip(input[:len(input)-3], input[3:]))
+part_1 = sum(b>a for a,b in zip(nums[:len(nums)-1], nums[1:]))
+part_2 = sum(sum(nums[i-3:i]) > sum(nums[i-4:i-1]) for i in range(4,len(nums)+1))
 
 print(part_1)
 print(part_2)
